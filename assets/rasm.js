fecacd2b77ee73f8a9f705749b32c33fa46b6a48
Stefan Bilharz <stefan@sbsoftware.de>
2018-08-21 23:45:58 +0200

    Initial commit of rasm.js

---
+++ assets/rasm.js
@@ -0,0 +1,14 @@
+import * as wasem from './wasem.js';
+
+const rasm_imports = {
+};
+
+export function load(path, custom_imports) {
+  let imports = {};
+  [rasm_imports, custom_imports].forEach(function (imports_container) {
+    for (const key in imports_container) {
+      imports[key] = imports_container[key];
+    }
+  });
+  return wasem.load(path, imports);
+}
